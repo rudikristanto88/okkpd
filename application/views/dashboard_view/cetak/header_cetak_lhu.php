89fdb7581799577757fac12f83821c97e0e1e75c
rudikristanto88 <rudi.kristanto@gmail.com>
2021-11-20 20:44:13 +0700

    20211120 perubahan tambahan uji mutu

---
+++ application/views/dashboard_view/cetak/header_cetak_lhu.php
@@ -0,0 +1,17 @@
+<table width=100%>
+<tr>
+    <td align="left" rowspan='3' width='80px'><img width='80px' src='<?php echo base_url()?>assets/dashboard/images/jateng.png'></td>
+</tr>
+<tr>
+    <td align="center"><p style="font-size:15px" align=center>PEMERINTAH PROVINSI JAWA TENGAH<br>
+       DINAS KETAHANAN PANGAN PROVINSI JAWA TENGAH<br>BALAI PENINGKATAN MUTU DAN KEAMANAN PANGAN
+     </p>
+     </td>
+     <td align="left" rowspan='3' width='80px'><img width='80px' src='<?php echo base_url()?>assets/dashboard/images/logo_kan.png'></td>
+
+</tr>
+<tr>
+    <td align="center"><p>Jl. Gatot Subroto - Ungaran, Telp. (024) 6922411 - 6923158, Fax. (024) 6921997</p></td>
+</tr>
+</table>
+<hr>
